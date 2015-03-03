d50e0d4762cdc727158c9f7d4357aec474bd7442
maste_000 <maste_000@128.4.126.105>
2015-03-03 02:24:40 -0500

    comment

---
+++ src/tuition/tuition.java
@@ -0,0 +1,20 @@
+package tuition;
+import java.util.scanner;
+public class tuition(int tuition){
+	
+	int tuition= 12342,
+	System.out.printIN("Enter percentage increase in tuition:"),
+	int percentage=sc.nextDouble();
+	{
+	
+	
+	for(int i = 0; i<4;i++);
+		FourYear=tuition*percentage;
+	}
+		return FourYear;
+	
+		
+		
+		
+
+}
